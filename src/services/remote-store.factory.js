6ec4e72cc261567c3226ae4f86efdd203b718e74
leseulsteve <leseulsteve@gmail.com>
2016-01-31 16:46:33 -0500

    Ajout des adresses de type qqchose/:sonId/autrechose

--- src/services/remote-store.factory.js
+++ src/services/remote-store.factory.js
@@ -3,40 +3,63 @@
 angular.module('leseulsteve.angular-mongoose').factory('RemoteStore',
   function ($http) {
 
+    
     function RemoteStore(apiUrl) {
       this.apiUrl = apiUrl;
     }
 
-    RemoteStore.prototype.find = function (query) {
+    function getCacheId(ressource) {
+      var splittedApiUrl = _.map(this.apiUrl.split('/'), function(urlPart) {
+        return _.startsWith(urlPart, ':') ? ressource[urlPart.substring(1)] : urlPart;
+      });
+      return splittedApiUrl.join('/') + '/' + ressource[idProp];
+    }
+
+    RemoteStore.prototype.find = function(query) {
+      var splittedApiUrl = _.map(this.apiUrl.split('/'), function(urlPart) {
+        if (_.startsWith(urlPart, ':')) {
+          var value = query[urlPart.substring(1)];
+          query = _.omit(query, urlPart.substring(1));
+          return value;
+        } else {
+          return urlPart;
+        }
+      });
       var headers = {
         params: query ? query : undefined
       };
-      return $http.get(this.apiUrl, headers).then(function (response) {
+      return $http.get(splittedApiUrl.join('/'), headers).then(function(response) {
         return response.data;
       });
     };
 
-    RemoteStore.prototype.findById = function (id) {
-      return $http.get(this.apiUrl + '/' + id).then(function (response) {
+    RemoteStore.prototype.findById = function(id) {
+      return $http.get(this.apiUrl + '/' + id).then(function(response) {
         return response.data;
       });
     };
 
-    RemoteStore.prototype.create = function (ressourceDef) {
-      return $http.post(this.apiUrl, ressourceDef).then(function (response) {
+    RemoteStore.prototype.create = function(ressourceDef) {
+      var splittedApiUrl = _.map(this.apiUrl.split('/'), function(urlPart) {
+        return _.startsWith(urlPart, ':') ? ressource[urlPart.substring(1)] : urlPart;
+      });
+      return $http.post(splittedApiUrl.join('/'), ressourceDef).then(function(response) {
         return response.data;
       });
     };
 
-    RemoteStore.prototype.update = function (ressource) {
-      return $http.put(this.apiUrl + '/' + ressource._id, ressource).then(function (response) {
+    RemoteStore.prototype.update = function(ressource) {
+      var identifiant = getCacheId(ressource);
+      return $http.put(identifiant, ressource).then(function(response) {
         return response.data;
       });
     };
 
-    RemoteStore.prototype.remove = function (ressource) {
-      return $http.delete(this.apiUrl + '/' + ressource._id);
+    RemoteStore.prototype.remove = function(ressource) {
+      var identifiant = getCacheId(ressource);
+      return $http.delete(identifiant);
     };
 
     return RemoteStore;
+
   });
